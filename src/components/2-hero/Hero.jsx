f6b3de790009d12b6bfbfed1e56dc30b0f32adc7
Moaz Karem <Moazkarem@gmail.com>
2024-07-02 07:43:43 +0300

    final portfolio

---
+++ src/components/2-hero/Hero.jsx
@@ -0,0 +1,79 @@
+import Lottie from "lottie-react";
+import "./hero.css";
+import devAnimation from "../../animation/dev.json";
+import { useRef } from "react";
+import { motion } from "framer-motion";
+
+const Hero = () => {
+  const lottieRef = useRef();
+
+  return (
+    <section className="hero flex">
+      <div className="left-section  ">
+        <div className="parent-avatar flex">
+          <motion.img
+            initial={{ transform: "scale(0)" }}
+            animate={{ transform: "scale(1.1)" }}
+            transition={{ damping: 2, type: "spring", stiffness: 100 }}
+            src="./me.jpeg"
+            className="avatar"
+            alt=""
+          />
+          <div className="icon-verified"></div>
+        </div>
+
+        <motion.h1
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 4 }}
+          className="title"
+        >
+          Moaz Karem , Computer sciense graduate , software engineer , front-end
+          develober
+        </motion.h1>
+
+        <p className="sub-title">
+          I am skilled Front End developer with experience in React Js ,
+          Javascript , typescript and Next js , Proficient in front end design
+          Framworks like Material Ui and Tailwind js , I enjoy taking complex
+          challenge and continously learning new language And framworks .
+          Dedicated and detail - oriented , I strive to deliver high quality
+          solutions that meet client’s
+        </p>
+
+        <div className="all-icons flex">
+          <a href="https://x.com/MoazKarem42">
+            <div className="icon icon-twitter"></div>
+          </a>
+
+          <a href="https://www.instagram.com/eng_moaz_karem/?hl=en">
+            <div className="icon icon-instagram"></div>
+          </a>
+
+          <a href="https://github.com/moazkarem">
+            <div className="icon icon-github"></div>
+          </a>
+
+          <a href="https://www.linkedin.com/in/moaz-karem-98a537234/">
+            <div className="icon icon-linkedin"></div>
+          </a>
+        </div>
+      </div>
+
+      <div className="right-section animation ">
+        <Lottie
+          lottieRef={lottieRef}
+          className=""
+          onLoadedImages={() => {
+            // @ts-ignore
+            // https://lottiereact.com/
+            lottieRef.current.setSpeed(0.5);
+          }}
+          animationData={devAnimation}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/components/3-main/myProjects.js
@@ -0,0 +1,82 @@
+export const myProjects = [
+  {
+    projectTitle: "Hospital System",
+    category: ["react"],
+    imgPath: "./images/005.jpg",
+    description:
+      "Hospital resourse management system and forcasting , to predict with resourse and stock availapility , exist two machinlearning model to make the prediction",
+    demo: "#",
+    repo: "https://github.com/moazkarem/Hospital_system",
+  },
+  {
+    projectTitle: "Fyrnival App",
+    category: ["react"],
+    imgPath: "./images/003.png",
+    description:
+      "Furnival is an ecommerce website concerned with selling furniture to make your dream home true",
+    demo: "#",
+    repo: "https://github.com/moazkarem/Furnival_app",
+  },
+  {
+    projectTitle: "Dokan Shop Fashion Store",
+    category: ["css"],
+    imgPath: "./images/001.png",
+    description:
+      "Professional multi-page design for a fashion store that makes it easy for the user to buy from home everything he wants from fashion",
+    demo: "https://dokanshop-3ed1kle3j-moazkarem.vercel.app/",
+    repo: "https://github.com/moazkarem/DokanShop",
+  },
+  {
+    projectTitle: "Shooping & Department Store",
+    category: ["react"],
+    imgPath: "./images/006.png",
+    description:
+      "A multi-page website for purchasing daily from all accessories needs. It contains a part for the laser to complete the purchase from home completely, and a part for the admin to control the store easily.",
+    demo: "#",
+    repo: "https://github.com/moazkarem/E_commerse",
+  },
+  {
+    projectTitle: "Admin Dashbord",
+    category: ["react"],
+    imgPath: "./images/004.png",
+    description:
+      "React admin dashbord , It displays a complete and continuous analysis of the business, showing the data in multiple forms, techniques, and very good performance.",
+    demo: "#",
+    repo: "https://github.com/moazkarem/Dashbord_react",
+  },
+  {
+    projectTitle: "Smart Todo App",
+    category: ["ts"],
+    imgPath: "./images/008.png",
+    description:
+      "Full stack To do App with strabi ,  react and typescript , full authuntication login and sign up , user can add , edit delet todo from his account",
+    demo: "#",
+    repo: "https://github.com/moazkarem/todo_app_ts",
+  },
+  {
+    projectTitle: "Clone Vs_code",
+    category: ["ts"],
+    imgPath: "./images/009.png",
+    description:
+      "clone some features from vs code using Typescript and react js",
+    demo: "#",
+    repo: "https://github.com/moazkarem/Vs_code_cloe",
+  },
+  {
+    projectTitle: "Head Phone Store ",
+    category: ["js"],
+    imgPath: "./images/002.png",
+    description:
+      "Fantastic landing page for mobile store and compter accessories",
+    demo: "https://landing-page-n2opqtd3h-moazkarem.vercel.app/",
+    repo: "https://github.com/moazkarem/Landing_page",
+  },
+  {
+    projectTitle: "X_O Game",
+    category: ["js"],
+    imgPath: "./images/007.png",
+    description: " xo game for entertainment and playing among friends ",
+    demo: "https://xo-game-nu-mauve.vercel.app/",
+    repo: "https://github.com/moazkarem/xo_game",
+  },
+];
